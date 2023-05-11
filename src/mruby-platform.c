e0b6e50c936bfeed497385d43f17b978931fbe07
Robert Rowe <1669080+CaptainJet@users.noreply.github.com>
2023-05-11 15:22:56 -0700

    Suppress warnings by explicitly checking defined

--- src/mruby-platform.c
+++ src/mruby-platform.c
@@ -9,22 +9,22 @@ mrb_value mrb_get_platform_symbol(mrb_state *mrb, mrb_value)
 #else
   return mrb_symbol_value(mrb_intern_lit(mrb, "windows_32"));
 #endif
-#elif __APPLE__
+#elif defined(__APPLE__)
 #include <TargetConditionals.h>
-#if TARGET_OS_IPHONE
+#if defined(TARGET_OS_IPHONE)
   return mrb_symbol_value(mrb_intern_lit(mrb, "ios"));
-#elif TARGET_OS_MAC
+#elif defined(TARGET_OS_MAC)
   return mrb_symbol_value(mrb_intern_lit(mrb, "macos"));
 #else
   return mrb_symbol_value(mrb_intern_lit(mrb, "apple"));
 #endif
-#elif __EMSCRIPTEN__
+#elif defined(__EMSCRIPTEN__)
   return mrb_symbol_value(mrb_intern_lit(mrb, "web"));
-#elif __ANDROID__
+#elif defined(__ANDROID__)
   return mrb_symbol_value(mrb_intern_lit(mrb, "android"));
-#elif __linux__
+#elif defined(__linux__)
   return mrb_symbol_value(mrb_intern_lit(mrb, "linux"));
-#elif __unix__
+#elif defined(__unix__)
   return mrb_symbol_value(mrb_intern_lit(mrb, "unix"));
 #elif defined(_POSIX_VERSION)
   return mrb_symbol_value(mrb_intern_lit(mrb, "posix"));
