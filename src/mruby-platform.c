ed40b5fb33db2fe0a504a474e68f6af38d894f16
Robert Rowe <1669080+CaptainJet@users.noreply.github.com>
2023-05-11 12:25:52 -0700

    Return symbol, finally

--- src/mruby-platform.c
+++ src/mruby-platform.c
@@ -1,35 +1,35 @@
 #include "mruby.h"
-#include "mruby/variable.h"
+#include "mruby/value.h"
 
-mrb_sym mrb_get_platform_symbol(mrb_state *mrb, mrb_value)
+mrb_value mrb_get_platform_symbol(mrb_state *mrb, mrb_value)
 {
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #ifdef _WIN64
-  return mrb_intern_lit(mrb, "windows_64");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "windows_64"));
 #else
-  return mrb_intern_lit(mrb, "windows_32");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "windows_32"));
 #endif
 #elif __APPLE__
 #include <TargetConditionals.h>
 #if TARGET_OS_IPHONE
-  return mrb_intern_lit(mrb, "ios");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "ios"));
 #elif TARGET_OS_MAC
-  return mrb_intern_lit(mrb, "macos");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "macos"));
 #else
-  return mrb_intern_lit(mrb, "apple");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "apple"));
 #endif
 #elif __EMSCRIPTEN__
-  return mrb_intern_lit(mrb, "web");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "web"));
 #elif __ANDROID__
-  return mrb_intern_lit(mrb, "android");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "android"));
 #elif __linux__
-  return mrb_intern_lit(mrb, "linux");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "linux"));
 #elif __unix__
-  return mrb_intern_lit(mrb, "unix");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "unix"));
 #elif defined(_POSIX_VERSION)
-  return mrb_intern_lit(mrb, "posix");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "posix"));
 #else
-  return mrb_intern_lit(mrb, "unknown");
+  return mrb_symbol_value(mrb_intern_lit(mrb, "unknown"));
 #endif
 }
 
